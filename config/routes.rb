2296aeb84139b9fa3feb866acc86348fa75dedab
Jacob(You-Hyun Hwang) <jacob.c@daumkakao.com>
2015-10-29 01:01:35 +0900

    5.10까지 완료

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :bulletins
-
-  resources :posts
+  resources :bulletins do
+    resources :posts
+  end
 
   root 'welcome#index'
   # get 'welcome/index'
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151027162954) do
+ActiveRecord::Schema.define(version: 20151028154228) do
 
-  create_table "bulletins", force: true do |t|
-    t.string   "title"
+  create_table "bulletins", force: :cascade do |t|
+    t.string   "title",       limit: 255
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "post_type"
   end
 
-  create_table "posts", force: true do |t|
-    t.string   "title"
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",       limit: 255
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
